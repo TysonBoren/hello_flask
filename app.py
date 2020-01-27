506cac62c9d65fafdf256302e71c083b151f420c
tyson boren <tyson.d.boren@gmail.com>
2020-01-27 16:47:28 -0700

    added post route

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
 
-#do not need this as of video 'Creating a SQLite Database in Flask with SQLAlchemy'. Dont understand why yet. 
-# @app.route('/')
-# def hello():
-#     return 'Hey Flask'
-
-#telling server where to put things. - 'daniel floyd'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
@@ -18,23 +12,37 @@ ma = Marshmallow(app)
 
 class Guide(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), unique = False)
-    content = db.Column(db.String(144), unique = False)
-    
+    title = db.Column(db.String(100), unique=False)
+    content = db.Column(db.String(144), unique=False)
+
     def __init__(self, title, content):
         self.title = title
         self.content = content
 
+
 class GuideSchema(ma.Schema):
-    class meta:
+    class Meta:
         fields = ('title', 'content')
+
 
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
-# from app import db. Wrote that in terminal and daniel said to make note of it. 
+
+# Endpoint to create a new guide
+@app.route('/guide', methods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+    new_guide = Guide(title, content)
+
+    db.session.add(new_guide)
+    db.session.commit()
+
+    guide = Guide.query.get(new_guide.id)
+
+    return guide_schema.jsonify(guide)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
-    
+    app.run(debug=True)    
