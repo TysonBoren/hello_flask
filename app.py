1dbe72fec901414dc866d2b08a069102bf4a1d06
tyson boren <tyson.d.boren@gmail.com>
2020-01-27 16:05:37 -0700

    Added functionality for creating a sqlite database with a basic guide schema. Currently has no routes

--- app.py
+++ app.py
@@ -5,9 +5,34 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hey Flask'
+#do not need this as of video 'Creating a SQLite Database in Flask with SQLAlchemy'. Dont understand why yet. 
+# @app.route('/')
+# def hello():
+#     return 'Hey Flask'
+
+#telling server where to put things. - 'daniel floyd'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Guide(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique = False)
+    content = db.Column(db.String(144), unique = False)
+    
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+class GuideSchema(ma.Schema):
+    class meta:
+        fields = ('title', 'content')
+
+guide_schema = GuideSchema()
+guides_schema = GuideSchema(many=True)
+# from app import db. Wrote that in terminal and daniel said to make note of it. 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Flask App
 
 The following project was built during my course with Bottega. It has full CRUD functionality.
+
+# https://github.com/bottega-code-school/hello-flask/tree/0f210cc30c2a46bd4ed3a2cd2f8ff43b6e42a7a6
+# http://flask-sqlalchemy.pocoo.org/2.3/
+# http://flask-marshmallow.readthedocs.io/en/latest/
+# https://marshmallow-sqlalchemy.readthedocs.io/en/latest/
